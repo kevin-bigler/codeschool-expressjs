c527694c8d41812f31791f4d2f2d1f408973b06c
Kevin Bigler <kevinbigler@gmail.com>
2017-05-09 17:42:18 -0700

    Add test-watch npm command. Add more tests for a new cities endpoint.

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 let express = require('express');
 let app = express();
 
-app.get('/', (request, response) => {
+app.get('/', function(request, response) {
 	// throw 'Error';
 	response.send('OK');
 });
 
+app.get('/cities', function(request, response) {
+	let cities = ['Mesa', 'Portland', 'Fargo'];
+	// response.json('OK');
+	response.json(cities);
+});
 
 // app.listen(3000);
 module.exports = app;	// do it this way, as a module, so that we can write tests using app
--- test.js
+++ test.js
@@ -1,18 +1,52 @@
 let request = require('supertest');
 let app = require('./app');
 
-describe('Requests to the root path', () => {
+describe('Requests to the root path', function() {
 
-	it('Returns a 200 status code', (done) => {
+	it('Returns a 200 status code', function(done) {
 
 		request(app)
-		.get('/')
-		.expect(200)
-		.end((error) => {
-			if (error) throw error;
-			done();
-		});
+			.get('/')
+			.expect(200)
+			.end(function(error) {
+				if (error) throw error;
+				done();
+			});
 
 	});
+
+});
+
+
+describe('Listing cities on /cities', function(){
+
+	it('Returns 200 status code', function(done){
+
+		request(app)
+			.get('/cities')
+			.expect(200, done);
+
+	});
+
+	it('Returns JSON format', function(done){
+
+		request(app)
+			.get('/cities')
+			.expect('Content-Type', /json/, done);
+			// .end(function(error) {
+			// 	if (error) throw error;
+			// 	done();
+			// });
+
+	});
+
+	it('Returns initial cities', function(done){
+
+		request(app)
+			.get('/cities')
+			.expect(JSON.stringify(['Mesa', 'Portland', 'Fargo']), done);
+	});
+
+
 
 });
